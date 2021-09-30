affc7bf79003d494ccdab89061be99b3ec0041fa
WilsonMaiangani <alifattahmaiangani@gmail.com>
2021-09-30 20:42:19 +0100

    Add projeto PDF join

---
+++ PDF Join/Program.cs
@@ -0,0 +1,88 @@
+﻿using iTextSharp.text;
+using iTextSharp.text.pdf;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace PDF_Join
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            byte[] file1 = File.ReadAllBytes(@"C:\Users\alifa\Downloads\PDF teste\CARTA.pdf");
+            byte[] file2 = File.ReadAllBytes(@"C:\Users\alifa\Downloads\PDF teste\A realização do Sonho.pdf");
+
+            Guid guid = Guid.NewGuid();            
+            var result = GerarDocumento("", file1, file2, guid);
+            salvarArquivoPDF(result);
+        }
+
+        public static byte[] GerarDocumento(string html, byte[] capa, byte[] contraCapa, Guid codigo)
+        {
+            List<byte[]> pdfs = new List<byte[]>();
+
+            if (capa != null)
+                pdfs.Add(capa);
+        
+            if (contraCapa != null)
+                pdfs.Add(contraCapa);
+
+            return ConcatenarArquivos(pdfs, codigo);
+        }
+
+        public static byte[] ConcatenarArquivos(List<byte[]> pdfs, Guid codigo)
+        {
+            using (var ms = new MemoryStream())
+            {
+                var outputDocument = new Document();
+                var writer = new PdfCopy(outputDocument, ms);
+                outputDocument.Open();
+
+                outputDocument.AddAuthor(codigo.ToString());
+
+                foreach (var doc in pdfs)
+                {
+                    var reader = new PdfReader(doc);
+                    for (var i = 1; i <= reader.NumberOfPages; i++)
+                    {
+                        writer.AddPage(writer.GetImportedPage(reader, i));
+                    }
+                    writer.FreeReader(reader);
+                    reader.Close();
+                }
+
+                writer.Close();
+                outputDocument.Close();
+                //var allPagesContent = ms.GetBuffer();
+                var allPagesContent = ms.ToArray();
+                ms.Flush();
+
+                return allPagesContent;
+            }
+        }
+
+        private static void salvarArquivoPDF(byte[] list)
+        {
+
+            var rdoGerado = list;
+            Guid guid = Guid.NewGuid();
+            //var pasta = string.Format("{0}\\{1}\\{2}\\RDO_{3}\\Revisao\\",
+            //            Configuracao.DiretorioArquivosRDO,
+            //            rdoPdf.DataCriacao.Year,
+            //            rdoPdf.DataCriacao.Month.ToString().PadLeft(2, '0'),
+            //            rdoPdf.Id.ToString().PadLeft(2, '0')
+            //            );
+            var pasta = @"C:\Users\alifa\Downloads\PDF teste\";
+            var nomeArquivo = guid.ToString().PadLeft(2, '0') + ".pdf";
+
+            bool exists = System.IO.Directory.Exists(pasta);
+
+            if (!exists)
+                System.IO.Directory.CreateDirectory(pasta);
+
+            File.WriteAllBytes(pasta + nomeArquivo, rdoGerado);
+        }
+
+    }
+}
---
+++ PDF join to PDF/Program.cs
@@ -0,0 +1,57 @@
+﻿using iTextSharp.text;
+using iTextSharp.text.pdf;
+using System;
+using System.IO;
+
+namespace PDF_join_to_PDF
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //if (args.Length == 0 || args[0] == "-h" || args[0] == "/h")
+            //{
+            //    Console.WriteLine("Welcome to MergeSpeedPASS. Created by Mladen Prajdic. Uses iTextSharp 5.4.5.0.");
+            //    Console.WriteLine("Tool to create a single SpeedPASS PDF from all downloaded generated PDFs.");
+            //    Console.WriteLine("");
+            //    Console.WriteLine("Example: MergeSpeedPASS.exe targetFileName sourceDir");
+            //    Console.WriteLine("         targetFileName = name of the new merged PDF file. Must include .pdf extension.");
+            //    Console.WriteLine("         sourceDir      = path to the dir containing downloaded attendee SpeedPASS PDFs");
+            //    Console.WriteLine("");
+            //    Console.WriteLine(@"Example: MergeSpeedPASS.exe MergedSpeedPASS.pdf d:\Downloads\SQLSaturdaySpeedPASSFiles");
+            //}
+            //else if (args.Length == 2)
+            //    CreateMergedPDF("join.pdf", @"C:\Users\alifa\Downloads\PDF teste\A realização do Sonho.pdf");
+
+            CreateMergedPDF("join.pdf", @"C:\Users\alifa\Downloads\PDF teste\");
+
+            Console.WriteLine("");
+            Console.WriteLine("Press any key to exit...");
+            Console.Read();
+        }
+
+        static void CreateMergedPDF(string targetPDF, string sourceDir)
+        {
+            using (FileStream stream = new FileStream(targetPDF, FileMode.Create))
+            {
+                Document pdfDoc = new Document(PageSize.A4);
+                PdfCopy pdf = new PdfCopy(pdfDoc, stream);
+                pdfDoc.Open();
+                var files = Directory.GetFiles(sourceDir);
+                Console.WriteLine("Merging files count: " + files.Length);
+                int i = 1;
+                foreach (string file in files)
+                {
+                    Console.WriteLine(i + ". Adding: " + file);
+                    pdf.AddDocument(new PdfReader(file));
+                    i++;
+                }
+
+                if (pdfDoc != null)
+                    pdfDoc.Close();
+
+                Console.WriteLine("SpeedPASS PDF merge complete.");
+            }
+        }
+    }
+}
